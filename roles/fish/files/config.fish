f533ec8295e00bcf7ef46a430796226103e27397
lyokato <lyo.kato@gmail.com>
2016-11-23 14:59:47 +0900

    adds fish setting, and removed zsh role from localhost.yml

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,11 @@
+set PATH /usr/local/bin /bin /usr/bin $PATH
+
+function chpwd --on-variable PWD
+  set -l cursor_pos (commandline --cursor)
+  # Only show directory listing in interactive mode when not tab completing
+  if test $cursor_pos -eq 0 ;and status --is-interactive
+    ll
+  end
+end
+
+# fish_update_completions
