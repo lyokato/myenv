57acf38270ce49062ea46db47b214772ca08206f
lyokato <lyo.kato@gmail.com>
2016-11-23 20:30:35 +0900

    updated fish settings: removed installation-error, and now, right-hand-side prompt shows git-branch

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -9,4 +9,27 @@ function chpwd --on-variable PWD
   end
 end
 
+ set fish_git_dirty_color red
+ set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_diff (git diff)
+
+  if test -n "$git_diff"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+    else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function fish_right_prompt
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+  if test -n "$git_dir"
+    printf '[%s]' (parse_git_branch)
+    else
+    printf ''
+  end
+end
+
 # fish_update_completions
